64ff48b15d3ec9775877dbc3a8a10a9c6db3f098
zdhuka <zoheb.dhuka@gmail.com>
2015-09-20 18:14:20 -0500

    MapReduce Completed with BestBuy Reviews

---
+++ PieAnalytics.Mongo/Review.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.JsonResult;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieAnalytics.Mongo
+{
+    public class Review
+    {
+
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+        /// <summary>
+        /// Insert API review data ino MongoDB
+        /// </summary>
+        /// <param name="jsondata">BsonArray as jsondata which contaons only reviews</param>
+        /// <param name="jobid">The ctaual user jonb id from RDBMS</param>
+        /// <param name="source">API source</param>
+        /// <returns>-1 if unsuccessfully, 1 if success</returns>
+        public int InsertReview(BsonDocument jsondata, string jobid, string source)
+        {
+            var d = new System.Configuration.AppSettingsReader().GetValue("MongoConnection", typeof(string));
+            _client = new MongoDB.Driver.MongoClient(d.ToString());
+            _database = _client.GetDatabase("PieAnalytics");
+            string collectionname = "Reviews";
+            var coll = _database.GetCollection<BsonDocument>(collectionname);
+            var data = new BsonDocument
+            {
+                {"jobid",jobid},
+                {"reviews",jsondata},
+                {"source",source}
+            };
+            var insertdata = coll.InsertOneAsync(data);
+            while (!insertdata.IsCompleted) ;
+            return insertdata.Exception == null ? 1 : -1;
+        }
+
+
+    }
+}
---
+++ PioneerApp.DataAnalysis/BestBuyAnalysis.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+namespace PioneerApp.DataAnalysis
+{
+    public class BestBuyAnalysis
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public int AnalyzeReviews(string jobid)
+        {
+            var d = new System.Configuration.AppSettingsReader().GetValue("MongoConnection", typeof(string));
+            _client = new MongoDB.Driver.MongoClient(d.ToString());
+            _database = _client.GetDatabase("PieAnalytics");
+            BsonJavaScript map = @"
+            function() {
+                var data = this.reviews.reviews;
+                var jid = this.jobid;
+                var src = this.source;
+                data.forEach(function(d){
+                    var key = { rating : d.rating, jobid : jid, source : src};
+                    var value = { count: 1 };
+                    emit(key, value);
+                });
+            }";
+
+            BsonJavaScript reduce = @"        
+            function(key, values) {
+                var result = {count : 0};
+                values.forEach(function(value){               
+                    result.count += value.count;
+                });
+                return result;
+            }";
+
+            var collection = _database.GetCollection<BsonDocument>("Reviews");
+            var options = new MapReduceOptions<BsonDocument, BsonDocument>();
+            options.OutputOptions = MapReduceOutputOptions.Merge("ReviewResults");
+            var filter = Builders<BsonDocument>.Filter.Eq("jobid", jobid);
+            options.Filter = filter;
+            var asyncresults = collection.MapReduceAsync<BsonDocument>(map, reduce, options);
+            while (!asyncresults.IsCompleted) ;
+            return asyncresults.Exception == null ? 1 : -1;
+        }
+
+    }
+}
