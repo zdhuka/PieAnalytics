91d841dc921f52523f651d933d865894f789fe5d
zdhuka <zoheb.dhuka@gmail.com>
2015-10-27 20:53:24 -0500

    Project completed

--- PieAnalytics.WindowsService/Service.cs
+++ PieAnalytics.WindowsService/Service.cs
@@ -60,7 +60,7 @@ namespace PieAnalytics.WindowsService
 
             // Set up a timer to trigger every minute.
             System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 60000/2; // 60/2 = 30 seconds
+            timer.Interval = 60000 / 2; // 60/2 = 30 seconds
             timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
 
             timer.Start();
@@ -72,37 +72,27 @@ namespace PieAnalytics.WindowsService
 
         public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
         {
-            //// TODO: Insert monitoring activities here.
-            //eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information);
-            //try
-            //{
-            //    string path = System.IO.Path.Combine("D:\\PieAnalytics\\PieAnalytics\\PieAnalytics.WindowsService\\bin\\", "Test.txt");
-            //    eventLog1.WriteEntry("Path:" + path, EventLogEntryType.Information);
-            //    if (!System.IO.File.Exists(path))
-            //    {
-            //        eventLog1.WriteEntry("Creating File", EventLogEntryType.Information, eventID++);
-            //        System.IO.File.Create(path).Close();
-            //    }
-            //    using (System.IO.StreamWriter file =
-            //    new System.IO.StreamWriter(path, true))
-            //    {
-            //        file.WriteLine("Success");
-            //        file.Close();
-            //    }
-            //}
-            //catch (Exception exp)
-            //{
-            //    eventLog1.WriteEntry(exp.Message, EventLogEntryType.Error);
-            //}
+            //WindowsService Starts here
+
+            System.Diagnostics.EventLog eventLog1 = new System.Diagnostics.EventLog();
+
+            if (!System.Diagnostics.EventLog.SourceExists("PieAnalyticsWindowsService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "PieAnalyticsWindowsService", "PieAnalyticsWindowsServiceLog");
+            }
+            eventLog1.Source = "PieAnalyticsWindowsServiceService";
+            eventLog1.Log = "PieAnalyticsWindowsServiceLog";
+            eventLog1.WriteEntry("Service Setting up");
 
 
 
             // TODO: Insert activities here.
-            eventLog1.WriteEntry("Fetching query data from the System", EventLogEntryType.Information);
-            var db = new PieAnalytics.DataEntity.PieAnalyticsEntities();
+            eventLog1.WriteEntry("Fetching query data from the System", System.Diagnostics.EventLogEntryType.Information);
+            //var db = new PieAnalytics.DataEntity.PieAnalyticsEntities();
             var _mongodb = new Review();
             int status = (int)JobStatus.Queued;
-            var jobentityquery = (from a in db.Jobs
+            var jobentityquery = (from a in _db.Jobs
                                   where a.Status == status
                                   orderby a.InsertDate descending
                                   select a);
@@ -112,12 +102,12 @@ namespace PieAnalytics.WindowsService
                 jobentity.Status = (int)JobStatus.Processing;
                 jobentity.UpdateDate = DateTime.Now;
                 //Update Status of Job in Porcesing
-                db.SaveChangesAsync();
-                eventLog1.WriteEntry("Updating record for that query in the System", EventLogEntryType.Information);
+                _db.SaveChangesAsync();
+                eventLog1.WriteEntry("Updating record for that query in the System", System.Diagnostics.EventLogEntryType.Information);
 
                 foreach (var d in jobentity.Keywords.Split(',').ToList())
                 {
-                    eventLog1.WriteEntry("Fetching social media data into the System", EventLogEntryType.Information);
+                    eventLog1.WriteEntry("Fetching social media data into the System", System.Diagnostics.EventLogEntryType.Information);
                     //Call the Rest API's 
                     // Call the API and fetch data
                     BestBuy bestbuy = new BestBuy();
@@ -125,22 +115,112 @@ namespace PieAnalytics.WindowsService
 
                     // Store the result in MongoDB
                     _mongodb.InsertReview(data, jobentity.JobID.ToString(), "BestBuy");
-                    eventLog1.WriteEntry("Finished fetching record for that data into the System", EventLogEntryType.Information);
+                    eventLog1.WriteEntry("Finished fetching record for that data into the System", System.Diagnostics.EventLogEntryType.Information);
                     //start MongoDB mapreduce
                 }
 
-                eventLog1.WriteEntry("Push sentiment analysis of that data within the System", EventLogEntryType.Information);
+                eventLog1.WriteEntry("Push sentiment analysis of that data within the System", System.Diagnostics.EventLogEntryType.Information);
                 // Call Map Reduce Program for Aggreating data collected using mapreduce
                 BestBuyAnalysis analysis = new BestBuyAnalysis();
                 analysis.AnalyzeReviews(jobentity.JobID.ToString());
                 //Update SQL database on sentiment analysis
+                List<BsonDocument> ratedData = analysis.GetResultAnalysedReviws(jobentity.JobID.ToString());
 
-
-                eventLog1.WriteEntry("Finished processing the query and data from/in/within the System", EventLogEntryType.Information);
+                JobResult bestbuy_result = null;
+                //For Best Buy
+                foreach (var d in ratedData)
+                {
+                    BsonDocument _id = d.GetElement("_id").Value.ToBsonDocument();
+                    if (bestbuy_result == null)
+                    {
+                        bestbuy_result = new JobResult();
+                        bestbuy_result.JobID = new Guid(_id.GetElement("jobid").Value.ToString());
+                        bestbuy_result.Source = (string)_id.GetElement("source").Value;
+                        bestbuy_result.InsertDate = DateTime.Now;
+                        bestbuy_result.UpdateDate = DateTime.Now;
+                    }
+                    if (bestbuy_result != null)
+                    {
+                        string rating = _id.GetElement("rating").Value.ToString();
+                        var value = d.GetElement("value").Value.ToBsonDocument();
+                        var count = value.GetElement("count").Value.ToString();
+                        if (rating == "1")
+                        {
+                            bestbuy_result.Rating1 = Convert.ToInt32(count);
+                        }
+                        if (rating == "2")
+                        {
+                            bestbuy_result.Rating2 = Convert.ToInt32(count);
+                        }
+                        if (rating == "3")
+                        {
+                            bestbuy_result.Rating3 = Convert.ToInt32(count);
+                        }
+                        if (rating == "4")
+                        {
+                            bestbuy_result.Rating4 = Convert.ToInt32(count);
+                        }
+                        if (rating == "5")
+                        {
+                            bestbuy_result.Rating5 = Convert.ToInt32(count);
+                        }
+                    }
+                }
+                _db.JobResults.Add(bestbuy_result);
+                eventLog1.WriteEntry("Finished processing the query and data from/in/within the System", System.Diagnostics.EventLogEntryType.Information);
                 // Update Job Status as completed
                 jobentity.Status = (int)JobStatus.Finished;
-                db.SaveChanges();
+                _db.SaveChanges();
+                eventLog1.WriteEntry("Finished exporting data and Job is completed", System.Diagnostics.EventLogEntryType.Information);
             }
+
+            //WindowsService Ends here
+
+
+            //// TODO: Insert activities here.
+            //eventLog1.WriteEntry("Fetching query data from the System", EventLogEntryType.Information);
+            //var db = new PieAnalytics.DataEntity.PieAnalyticsEntities();
+            //var _mongodb = new Review();
+            //int status = (int)JobStatus.Queued;
+            //var jobentityquery = (from a in db.Jobs
+            //                      where a.Status == status
+            //                      orderby a.InsertDate descending
+            //                      select a);
+            //Job jobentity = jobentityquery.FirstOrDefault();
+            //if (jobentity != null)
+            //{
+            //    jobentity.Status = (int)JobStatus.Processing;
+            //    jobentity.UpdateDate = DateTime.Now;
+            //    //Update Status of Job in Porcesing
+            //    db.SaveChangesAsync();
+            //    eventLog1.WriteEntry("Updating record for that query in the System", EventLogEntryType.Information);
+
+            //    foreach (var d in jobentity.Keywords.Split(',').ToList())
+            //    {
+            //        eventLog1.WriteEntry("Fetching social media data into the System", EventLogEntryType.Information);
+            //        //Call the Rest API's 
+            //        // Call the API and fetch data
+            //        BestBuy bestbuy = new BestBuy();
+            //        BsonDocument data = bestbuy.GetReview(d, "");
+
+            //        // Store the result in MongoDB
+            //        _mongodb.InsertReview(data, jobentity.JobID.ToString(), "BestBuy");
+            //        eventLog1.WriteEntry("Finished fetching record for that data into the System", EventLogEntryType.Information);
+            //        //start MongoDB mapreduce
+            //    }
+
+            //    eventLog1.WriteEntry("Push sentiment analysis of that data within the System", EventLogEntryType.Information);
+            //    // Call Map Reduce Program for Aggreating data collected using mapreduce
+            //    BestBuyAnalysis analysis = new BestBuyAnalysis();
+            //    analysis.AnalyzeReviews(jobentity.JobID.ToString());
+            //    //Update SQL database on sentiment analysis
+
+
+            //    eventLog1.WriteEntry("Finished processing the query and data from/in/within the System", EventLogEntryType.Information);
+            //    // Update Job Status as completed
+            //    jobentity.Status = (int)JobStatus.Finished;
+            //    db.SaveChanges();
+            //}
         }
 
         protected override void OnStop()
